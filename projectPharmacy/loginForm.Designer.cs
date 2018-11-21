5df07e968a8a8be8f455757aa9cda5c0d002a301
K00211816 <K00211816@student.lit.ie>
2018-11-21 14:40:06 +0000

    Login and repo creation

---
+++ projectPharmacy/loginForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace projectPharmacy
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.loginPassword = new System.Windows.Forms.TextBox();
+            this.loginUsername = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(275, 245);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // loginPassword
+            // 
+            this.loginPassword.Location = new System.Drawing.Point(263, 179);
+            this.loginPassword.Name = "loginPassword";
+            this.loginPassword.Size = new System.Drawing.Size(100, 20);
+            this.loginPassword.TabIndex = 1;
+            this.loginPassword.TextChanged += new System.EventHandler(this.loginPassword_TextChanged);
+            // 
+            // loginUsername
+            // 
+            this.loginUsername.Location = new System.Drawing.Point(263, 144);
+            this.loginUsername.Name = "loginUsername";
+            this.loginUsername.Size = new System.Drawing.Size(100, 20);
+            this.loginUsername.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(275, 216);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::projectPharmacy.Properties.Resources.Pharmacy_Green_Cross_svg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.loginUsername);
+            this.Controls.Add(this.loginPassword);
+            this.Controls.Add(this.exitButton);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox loginPassword;
+        private System.Windows.Forms.TextBox loginUsername;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ projectPharmacy/loginForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectPharmacy{
+    public partial class loginForm : Form{
+        public loginForm(){
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginPassword_TextChanged(object sender, EventArgs e){}
+
+        private void loginButton_Click(object sender, EventArgs e){
+            // this is where we go to the server and check whether this loginUsername.Text is in the database and then that loginPassword.Text match.
+            if (loginUsername.Text == "Corey" && loginPassword.Text == "Passw0rd"){
+                MessageBox.Show("Welcome " + loginUsername.Text);
+                loginUsername.Text = "";
+                loginPassword.Text = "";
+                loginUsername.Select();
+            }
+            else{
+                MessageBox.Show("Invalid name or password");
+                loginUsername.Text = "";
+                loginPassword.Text = "";
+                loginUsername.Select();
+            }
+        }
+    }
+}
